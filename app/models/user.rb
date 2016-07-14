35698cbdbb0477a7569ed84599c4a17fdf076dcc
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-14 17:54:23 -0400

    adds exclude allergies method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,15 @@ class User < ApplicationRecord
   has_one :author
   has_one :reader
 
-
+  def filter_allergies
+  	allergy_array = self.allergies.split(/[\s,']/).reject { |c| c.empty? }
+  	@dangerous_food = []
+  	allergy_array.select do |allergy|
+  		@dangerous_food << Recipe.joins(:ingredients).where('ingredients.name != ?', '#{allergy}').to_a.flatten.uniq.first
+  	end
+  	@dangerous_food.uniq
+  	@safe_food = Recipe.all.to_a - @dangerous_food
+  end
 
 
 
