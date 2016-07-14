b2f5f19957047661df0c196ff90e6a221271e660
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-14 15:56:30 -0400

    completes most_liked rspec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe Author, type: :model do
 	describe '#most_liked' do
 		let(:author) { Author.create(user: User.create(name:'avi', password:'flatiron'))}
 		let(:recipes) {FactoryGirl.create_list(:recipe, 3, author_id: author.id) }
-		let(:bookmarks_1) {FactoryGirl.create_list(:bookmark, 2, recipe: recipes[0], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))}
-		let(:bookmarks_2) {FactoryGirl.create_list(:bookmark, 3, recipe: recipes[1], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))}
-		let(:bookmarks_3) {FactoryGirl.create_list(:bookmark, 1, recipe: recipes[2], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))}
+		before do  
+			FactoryGirl.create_list(:bookmark, 2, recipe: recipes[0], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))
+			FactoryGirl.create_list(:bookmark, 3, recipe: recipes[1], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))
+			FactoryGirl.create_list(:bookmark, 1, recipe: recipes[2], reader: Reader.create(user: User.create(name:'tom', password:'jerry')))
+		end
 		it 'shows the most_liked for an author' do
 			author.recipes = recipes
+
 			expect(author.most_liked).to eq(author.recipes.second)
 		end
 	end
