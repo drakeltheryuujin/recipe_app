2b27e729536d7297bd45469c1e382b6c2c0bc277
Dan <dandanberry@gmail.com>
2016-07-14 13:47:58 -0400

    WIP test for #most_liked

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_one :author
   has_one :reader
 
+  def test
+  	self.author.recipes.first
+  end
+
 
   def search_by_fav_cuisine
     faves = Recipe.all.search(self.fav_cuisine)
--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,4 +2,3 @@ FactoryGirl.define do
   factory :recipe do
     title "Tuna Salad"
   end
-end
