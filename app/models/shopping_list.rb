2dda29e63716d1131d6cea63cc0a790f38c0874c
abillig <avrambillig@gmail.com>
2016-07-15 20:06:28 -0400

    tied shopping list to individual user

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -7,9 +7,19 @@ class ShoppingList < ApplicationRecord
 		self.line_items << LineItem.create(ingredient_id: ingredient.id,shopping_list_id: self.id)
 	end
 
+<<<<<<< 01558d8b5039a9d189edb63869cbb152822f44f4
   	def count_same_ingredients
     	self.class.joins(:ingredients).group('ingredients.name').count('ingredients.name').sort.reverse
   	end
   	
+=======
+  def count_same_ingredients
+    self.class.joins(:ingredients).where(user_id: self.user.id).group('ingredients.name').count('ingredients.name').sort.reverse
+  end
+
+	# def count_same_ingredient(target_ingredient)
+	# 	ShoppingList.joins(line_items: :ingredient).count("ingredient.name").where("ingredient.name = #{target_ingredient.name}")	
+	# end
+>>>>>>> tied shopping list to individual user
 end
 
