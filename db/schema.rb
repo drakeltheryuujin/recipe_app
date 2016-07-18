e9f800f1df68486e4e86d8f9f91dae1a571c8d47
Dan <dandanberry@gmail.com>
2016-07-18 16:54:49 -0400

    WIP client and adapter for store finder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717013024) do
+ActiveRecord::Schema.define(version: 20160718181920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,18 @@ ActiveRecord::Schema.define(version: 20160717013024) do
   create_table "shopping_lists", force: :cascade do |t|
     t.integer "user_id"
     t.float   "total_price"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "storeID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "phone"
   end
 
   create_table "users", force: :cascade do |t|
